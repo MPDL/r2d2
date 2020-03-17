8578e20a9150f0de9980426ae55797445a338b92
haarli <haarlaender@mpdl.mpg.de>
2020-03-17 09:51:55 +0100

    Some first exceptions

---
+++ src/main/java/de/mpg/mpdl/r2d2/exceptions/InvalidStateException.java
@@ -0,0 +1,26 @@
+package de.mpg.mpdl.r2d2.exceptions;
+
+public class InvalidStateException extends R2d2ApplicationException {
+
+	public InvalidStateException() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public InvalidStateException(String message, Throwable cause, boolean enableSuppression,
+			boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+		// TODO Auto-generated constructor stub
+	}
+
+	public InvalidStateException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public InvalidStateException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+	
+}
---
+++ src/main/java/de/mpg/mpdl/r2d2/exceptions/OptimisticLockingException.java
@@ -0,0 +1,26 @@
+package de.mpg.mpdl.r2d2.exceptions;
+
+public class OptimisticLockingException extends R2d2ApplicationException {
+
+	public OptimisticLockingException() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public OptimisticLockingException(String message, Throwable cause, boolean enableSuppression,
+			boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+		// TODO Auto-generated constructor stub
+	}
+
+	public OptimisticLockingException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public OptimisticLockingException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+	
+}
