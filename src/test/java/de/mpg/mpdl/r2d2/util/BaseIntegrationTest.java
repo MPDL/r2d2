5825f9472b8f451c6f9ed4a623fe556d6978ab7a
helkv <helk@mpdl.mpg.de>
2020-04-06 16:14:54 +0200

    Clear Context after each Integration Test (@DirtiesContext)
    - Clears the context after each test that extends BaseIntegrationTest. A
    new context is automatically created before each subsequent test.
    - Clears the test DB by: Clear context -> create new context (startup)
    -> DB is created on startup (spring.jpa.hibernate.ddl-auto=create)

--- src/test/java/de/mpg/mpdl/r2d2/util/BaseIntegrationTest.java
+++ src/test/java/de/mpg/mpdl/r2d2/util/BaseIntegrationTest.java
@@ -2,11 +2,14 @@ package de.mpg.mpdl.r2d2.util;
 
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.annotation.DirtiesContext.ClassMode;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
+@DirtiesContext(classMode = ClassMode.AFTER_CLASS)
 @ContextConfiguration(initializers = {DataBaseLauncher.Initializer.class, SearchEngineLauncher.Initializer.class})
 public abstract class BaseIntegrationTest {
 
