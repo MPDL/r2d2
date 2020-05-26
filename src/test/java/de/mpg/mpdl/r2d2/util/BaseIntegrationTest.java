5d3eee99ecae632a840b381b143e5e4e696ab494
helkv <helk@mpdl.mpg.de>
2020-05-26 09:27:58 +0200

    Add Mock of the ObjectStoreRepository for the Integration Tests

--- src/test/java/de/mpg/mpdl/r2d2/util/BaseIntegrationTest.java
+++ src/test/java/de/mpg/mpdl/r2d2/util/BaseIntegrationTest.java
@@ -2,10 +2,13 @@ package de.mpg.mpdl.r2d2.util;
 
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.annotation.DirtiesContext.ClassMode;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import de.mpg.mpdl.r2d2.service.storage.SwiftObjectStoreRepository;
 
 @ExtendWith({SpringExtension.class, DeleteSearchIndexExtension.class})
 @SpringBootTest
@@ -14,5 +17,10 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 public abstract class BaseIntegrationTest {
 
   //TODO: Stop DB/ES container after test?
+
+  //TODO: Enhance/Adapt the mocking of the cloud storage access
+
+  @MockBean
+  private SwiftObjectStoreRepository objectStoreRepository;
 
 }
