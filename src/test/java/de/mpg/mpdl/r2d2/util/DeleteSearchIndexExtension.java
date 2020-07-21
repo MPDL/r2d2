2a68486311ceabdfea1cedc1f414177bc0c19dd2
helkv <helk@mpdl.mpg.de>
2020-07-21 09:46:01 +0200

    Adapt DeleteSearchIndexExtension: Deleting all content of the indices
    -> Before all indices were deleted, not the content of the indices.
    -> Now all data of all indices are deleted
    -> TODO: Wait for the (asynch) deletion to finish before starting the
    next test OR use other approach

--- src/test/java/de/mpg/mpdl/r2d2/util/DeleteSearchIndexExtension.java
+++ src/test/java/de/mpg/mpdl/r2d2/util/DeleteSearchIndexExtension.java
@@ -1,10 +1,13 @@
 package de.mpg.mpdl.r2d2.util;
 
 import org.apache.http.HttpHost;
-import org.elasticsearch.action.admin.indices.delete.DeleteIndexRequest;
 import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.RestClient;
 import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.client.indices.GetIndexRequest;
+import org.elasticsearch.client.indices.GetIndexResponse;
+import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.index.reindex.DeleteByQueryRequest;
 import org.junit.jupiter.api.extension.AfterEachCallback;
 import org.junit.jupiter.api.extension.ExtensionContext;
 
@@ -12,12 +15,23 @@ public class DeleteSearchIndexExtension implements AfterEachCallback {
 
   @Override
   public void afterEach(ExtensionContext context) throws Exception {
+
     RestHighLevelClient elasticSearchClient =
         new RestHighLevelClient(RestClient.builder(HttpHost.create(SearchEngineLauncher.ELASTIC_SEARCH_CONTAINER.getHttpHostAddress())));
+    //Alternatively get the HostAddress from the environment/contex prperty: elasticsearch.url
 
-    elasticSearchClient.indices().delete(new DeleteIndexRequest("_all"), RequestOptions.DEFAULT);
+    //TODO: Delete and recreate all indices, instead of delete all data of the indices (which is slower)
+
+    GetIndexResponse getIndexResponse = elasticSearchClient.indices().get(new GetIndexRequest("_all"), RequestOptions.DEFAULT);
+    DeleteByQueryRequest deleteAllDocumentsInAllIndicesRequest = new DeleteByQueryRequest(getIndexResponse.getIndices());
+    deleteAllDocumentsInAllIndicesRequest.setQuery(QueryBuilders.matchAllQuery());
+    elasticSearchClient.deleteByQuery(deleteAllDocumentsInAllIndicesRequest, RequestOptions.DEFAULT);
+
+    //FIXME: The deletion runs asynchronously: Wait for the deletion of the indices to finish 
+    //(By waiting for the deletion task to complete or by waiting for the elastic search index status)
 
     elasticSearchClient.close();
+
   }
 
 }
