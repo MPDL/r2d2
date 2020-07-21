b4a7a5664039f9f9255bdc181bc2d8ba7eaa73c1
helkv <helk@mpdl.mpg.de>
2020-07-21 10:43:56 +0200

    Adapt DeleteSearchIndexExtension: Delete and recreate all indices
    -> Delete all indices and recreate them, instead of deleting all data of
    the indices (which is slower)

--- src/test/java/de/mpg/mpdl/r2d2/util/DeleteSearchIndexExtension.java
+++ src/test/java/de/mpg/mpdl/r2d2/util/DeleteSearchIndexExtension.java
@@ -1,13 +1,21 @@
 package de.mpg.mpdl.r2d2.util;
 
+import java.util.List;
+import java.util.Map;
+
 import org.apache.http.HttpHost;
+import org.elasticsearch.action.admin.indices.alias.Alias;
+import org.elasticsearch.action.admin.indices.delete.DeleteIndexRequest;
 import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.RestClient;
 import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.client.indices.CreateIndexRequest;
 import org.elasticsearch.client.indices.GetIndexRequest;
 import org.elasticsearch.client.indices.GetIndexResponse;
-import org.elasticsearch.index.query.QueryBuilders;
-import org.elasticsearch.index.reindex.DeleteByQueryRequest;
+import org.elasticsearch.cluster.metadata.AliasMetaData;
+import org.elasticsearch.cluster.metadata.MappingMetaData;
+import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.common.settings.Settings.Builder;
 import org.junit.jupiter.api.extension.AfterEachCallback;
 import org.junit.jupiter.api.extension.ExtensionContext;
 
@@ -18,17 +26,39 @@ public class DeleteSearchIndexExtension implements AfterEachCallback {
 
     RestHighLevelClient elasticSearchClient =
         new RestHighLevelClient(RestClient.builder(HttpHost.create(SearchEngineLauncher.ELASTIC_SEARCH_CONTAINER.getHttpHostAddress())));
-    //Alternatively get the HostAddress from the environment/contex prperty: elasticsearch.url
 
-    //TODO: Delete and recreate all indices, instead of delete all data of the indices (which is slower)
+    //TODO: Maybe read settings and mappings from the resource files, instead of getting them from the already created indices!?
 
     GetIndexResponse getIndexResponse = elasticSearchClient.indices().get(new GetIndexRequest("_all"), RequestOptions.DEFAULT);
-    DeleteByQueryRequest deleteAllDocumentsInAllIndicesRequest = new DeleteByQueryRequest(getIndexResponse.getIndices());
-    deleteAllDocumentsInAllIndicesRequest.setQuery(QueryBuilders.matchAllQuery());
-    elasticSearchClient.deleteByQuery(deleteAllDocumentsInAllIndicesRequest, RequestOptions.DEFAULT);
+    String[] indices = getIndexResponse.getIndices();
+    Map<String, List<AliasMetaData>> aliases = getIndexResponse.getAliases();
+    Map<String, MappingMetaData> mappings = getIndexResponse.getMappings();
+    Map<String, Settings> settings = getIndexResponse.getSettings();
 
-    //FIXME: The deletion runs asynchronously: Wait for the deletion of the indices to finish 
-    //(By waiting for the deletion task to complete or by waiting for the elastic search index status)
+    elasticSearchClient.indices().delete(new DeleteIndexRequest("_all"), RequestOptions.DEFAULT);
+
+    for (String index : indices) {
+      List<AliasMetaData> indexAliases = aliases.get(index);
+      MappingMetaData indexMappingMetaData = mappings.get(index);
+      Settings indexSettings = settings.get(index);
+
+      //How to get the original settings of an index (without the auto-created setting: creation_date, provided_name, uuid, version.created)?
+      //FIXME: Generalize index recreation: Do all indices have the following auto-created settings and the exact same setting-keys (like: index.creation_date)?
+
+      //Remove all implicit/index-specific settings (before using the settings for recreation):
+      Builder settingsBuilder = Settings.builder().put(indexSettings);
+      settingsBuilder.remove("index.creation_date");
+      settingsBuilder.remove("index.provided_name");
+      settingsBuilder.remove("index.uuid");
+      settingsBuilder.remove("index.version.created");
+      indexSettings = settingsBuilder.build();
+
+      CreateIndexRequest createIndexRequest = new CreateIndexRequest(index).settings(indexSettings);
+      indexAliases.forEach(indexAlias -> createIndexRequest.alias(new Alias(indexAlias.alias())));
+      createIndexRequest.mapping(indexMappingMetaData.getSourceAsMap());
+
+      elasticSearchClient.indices().create(createIndexRequest, RequestOptions.DEFAULT);
+    }
 
     elasticSearchClient.close();
 
