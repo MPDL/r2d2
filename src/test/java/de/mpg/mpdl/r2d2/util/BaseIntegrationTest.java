57d09a3d8c845d20ad574f52a6a2bae81bf7e663
helkv <helk@mpdl.mpg.de>
2021-01-26 09:10:52 +0100

    Add comment (about Starting/Stopping Testcontainers) to BaseIntegrationTest

--- src/test/java/de/mpg/mpdl/r2d2/util/BaseIntegrationTest.java
+++ src/test/java/de/mpg/mpdl/r2d2/util/BaseIntegrationTest.java
@@ -13,7 +13,11 @@ import de.mpg.mpdl.r2d2.service.storage.SwiftObjectStoreRepository;
 @ContextConfiguration(initializers = {DataBaseLauncher.Initializer.class, SearchEngineLauncher.Initializer.class})
 public abstract class BaseIntegrationTest {
 
-  //TODO: Stop DB/ES container after test?
+  //Starting/Stopping the Testcontainers:
+  // The singleton container is started only once when the base class is loaded.
+  // The container can then be used by all inheriting test classes.
+  // At the end of the test suite the Ryuk container that is started by Testcontainers core will take care of stopping the singleton container.
+  // see: https://www.testcontainers.org/test_framework_integration/manual_lifecycle_control/
 
   //TODO: Enhance/Adapt the mocking of the cloud storage access
 
