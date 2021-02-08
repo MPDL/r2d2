537cfdfa657cb35c686902cd5e32b3569243e162
wilhelmfrank <frank@mpdl.mpg.de>
2021-02-08 18:51:10 +0100

    User auditing return empty if anonymous

--- src/main/java/de/mpg/mpdl/r2d2/db/audit/CurrentUserAuditing.java
+++ src/main/java/de/mpg/mpdl/r2d2/db/audit/CurrentUserAuditing.java
@@ -3,6 +3,7 @@ package de.mpg.mpdl.r2d2.db.audit;
 import java.util.Optional;
 
 import org.springframework.data.domain.AuditorAware;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 
@@ -14,8 +15,13 @@ public class CurrentUserAuditing implements AuditorAware<UserAccount> {
   @Override
   public Optional<UserAccount> getCurrentAuditor() {
     Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-
+    /*
+    System.out.println("AUTHENTICATION: " + authentication);
     if (authentication == null || !authentication.isAuthenticated()) {
+      return Optional.empty();
+    }
+    */
+    if (authentication == null || authentication instanceof AnonymousAuthenticationToken) {
       return Optional.empty();
     }
     UserAccount ua = ((R2D2Principal) authentication.getPrincipal()).getUserAccount();
--- src/test/java/de/mpg/mpdl/r2d2/util/BaseIntegrationTest.java
+++ src/test/java/de/mpg/mpdl/r2d2/util/BaseIntegrationTest.java
@@ -19,10 +19,10 @@ import de.mpg.mpdl.r2d2.service.storage.SwiftObjectStoreRepository;
  *
  * <p>
  * Starting/Stopping the Testcontainers: <br>
- * A singleton container is started only once when the base class is loaded.
- * The container can then be used by all inheriting test classes.
- * At the end of the test suite the Ryuk container that is started by Testcontainers core will take care of stopping the singleton container.
- * see: https://www.testcontainers.org/test_framework_integration/manual_lifecycle_control/
+ * A singleton container is started only once when the base class is loaded. The container can then
+ * be used by all inheriting test classes. At the end of the test suite the Ryuk container that is
+ * started by Testcontainers core will take care of stopping the singleton container. see:
+ * https://www.testcontainers.org/test_framework_integration/manual_lifecycle_control/
  * </p>
  *
  */
