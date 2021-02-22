beb8ea3bae7b4751d7264a2375fc8f37bee19c03
helkv <helk@mpdl.mpg.de>
2021-02-22 09:47:12 +0100

    RD-8 Add additional assertions to DoiDataCreatorTest

--- src/test/java/de/mpg/mpdl/r2d2/transformation/doi/DoiDataCreatorTest.java
+++ src/test/java/de/mpg/mpdl/r2d2/transformation/doi/DoiDataCreatorTest.java
@@ -2,27 +2,35 @@ package de.mpg.mpdl.r2d2.transformation.doi;
 
 import de.mpg.mpdl.r2d2.exceptions.R2d2TechnicalException;
 import de.mpg.mpdl.r2d2.model.DatasetVersion;
+import de.mpg.mpdl.r2d2.transformation.doi.model.DoiAttributes;
 import de.mpg.mpdl.r2d2.transformation.doi.model.DoiData;
 import de.mpg.mpdl.r2d2.util.testdata.builder.DatasetVersionBuilder;
+import io.netty.handler.codec.base64.Base64Decoder;
+import org.assertj.core.api.ThrowableAssert;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.mapstruct.factory.Mappers;
 import org.springframework.mock.env.MockEnvironment;
 
+import java.util.Base64;
+
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatCode;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class DoiDataCreatorTest {
 
   private DoiDataCreator doiDataCreator;
 
+  private String prefix = "Test-Prefix";
+
   @BeforeAll
   void setupDoiDataCreator(){
     DoiMetadataMapper doiMetadataMapper = Mappers.getMapper(DoiMetadataMapper.class);
     DoiMetadataXmlConverter doiMetadataXmlConverter = new DoiMetadataXmlConverter();
     MockEnvironment env = new MockEnvironment();
-    env.setProperty("datacite.doi.prefix", "Prefix");
+    env.setProperty("datacite.doi.prefix", prefix);
 
     doiDataCreator = new DoiDataCreator(doiMetadataMapper, doiMetadataXmlConverter, env);
   }
@@ -38,7 +46,10 @@ public class DoiDataCreatorTest {
     //Then
     assertThat(doiData).extracting(DoiData::getType).isEqualTo(DoiData.DoiType.DOIS);
     assertThat(doiData).extracting(DoiData::getAttributes).isNotNull();
-    //TODO Add more assertions
+    assertThat(doiData.getAttributes()).extracting(DoiAttributes::getPrefix).isEqualTo(this.prefix);
+//    assertThat(doiData.getAttributes()).extracting(DoiAttributes::getUrl).isEqualTo();
+    //Check metadataXML is Base64 encoded:
+    assertThatCode(() -> Base64.getDecoder().decode(doiData.getAttributes().getXml())).doesNotThrowAnyException();
   }
 
   @Test
@@ -52,7 +63,10 @@ public class DoiDataCreatorTest {
     //Then
     assertThat(doiData).extracting(DoiData::getType).isNull();
     assertThat(doiData).extracting(DoiData::getAttributes).isNotNull();
-    //TODO Add more assertions
+    assertThat(doiData.getAttributes()).extracting(DoiAttributes::getEvent).isEqualTo(DoiAttributes.DoiEvent.PUBLISH);
+    //    assertThat(doiData.getAttributes()).extracting(DoiAttributes::getUrl).isEqualTo();
+    //Check metadataXML is Base64 encoded:
+    assertThatCode(() -> Base64.getDecoder().decode(doiData.getAttributes().getXml())).doesNotThrowAnyException();
   }
 
 }
