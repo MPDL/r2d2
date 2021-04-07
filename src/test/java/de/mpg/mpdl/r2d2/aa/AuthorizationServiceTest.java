7b2ee5f4385b26947d10af8de62fc7d15669f6d9
helkv <helk@mpdl.mpg.de>
2021-04-07 12:29:35 +0200

    Extend AuthorizationServiceTest (Tests for FileUploadService)
    - Add multiple Authorization-Testcases for FileUploadService

--- src/test/java/de/mpg/mpdl/r2d2/aa/AuthorizationServiceTest.java
+++ src/test/java/de/mpg/mpdl/r2d2/aa/AuthorizationServiceTest.java
@@ -6,10 +6,12 @@ import de.mpg.mpdl.r2d2.db.UserAccountRepository;
 import de.mpg.mpdl.r2d2.exceptions.AuthorizationException;
 import de.mpg.mpdl.r2d2.model.Dataset;
 import de.mpg.mpdl.r2d2.model.DatasetVersion;
+import de.mpg.mpdl.r2d2.model.File;
 import de.mpg.mpdl.r2d2.model.ReviewToken;
 import de.mpg.mpdl.r2d2.model.aa.R2D2Principal;
 import de.mpg.mpdl.r2d2.model.aa.UserAccount;
 import de.mpg.mpdl.r2d2.service.impl.DatasetVersionServiceDbImpl;
+import de.mpg.mpdl.r2d2.service.impl.FileUploadService;
 import de.mpg.mpdl.r2d2.util.testdata.builder.*;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -49,11 +51,11 @@ class AuthorizationServiceTest {
   @Mock
   private ReviewTokenRepository reviewTokenRepository;
 
-  private static Stream<Arguments> provideArgumentsForAuthorization() {
+  private static Stream<Arguments> provideArgumentsForDatasetVersionServiceAuthorization() {
     String datasetVersionServiceName = DatasetVersionServiceDbImpl.class.getCanonicalName();
 
     return Stream.of(
-        //Arguments: authorized, serviceName, methodName, State (of the DatasetVersion), isCreator(?), Grant (Role for the DatasetVersion), hasReviewToken
+        //Arguments: authorized, serviceName, methodName, State (of the DatasetVersion), isCreator(?), Grant (Role for the Dataset), hasReviewToken
         Arguments.of(true, datasetVersionServiceName, "create", PRIVATE, false, USER, false),
         Arguments.of(true, datasetVersionServiceName, "create", PRIVATE, false, ADMIN, false),
         Arguments.of(false, datasetVersionServiceName, "get", PRIVATE, false, USER, false),
@@ -92,13 +94,29 @@ class AuthorizationServiceTest {
         Arguments.of(false, datasetVersionServiceName, "createReviewToken", PUBLIC, true, USER, false));
   }
 
+  private static Stream<Arguments> provideArgumentsForFileUploadServiceAuthorization() {
+    String fileUploadServiceName = FileUploadService.class.getCanonicalName();
+
+    return Stream.of(
+        //Arguments: authorized, serviceName, methodName, State (of the File), isCreator(of the File), isCreator(of the Dataset), Grant (Role for the Dataset), hasReviewToken
+        Arguments.of(true, fileUploadServiceName, "upload", File.UploadState.INITIATED, false, false, USER, false),
+        Arguments.of(true, fileUploadServiceName, "upload", File.UploadState.INITIATED, false, false, ADMIN, false),
+        Arguments.of(false, fileUploadServiceName, "get", File.UploadState.COMPLETE, false, false, USER, false),
+        Arguments.of(true, fileUploadServiceName, "get", File.UploadState.PUBLIC, false, false, USER, false),
+        Arguments.of(true, fileUploadServiceName, "get", File.UploadState.COMPLETE, false, false, ADMIN, false),
+        Arguments.of(true, fileUploadServiceName, "get", File.UploadState.COMPLETE, true, false, USER, false),
+        Arguments.of(true, fileUploadServiceName, "get", File.UploadState.COMPLETE, false, true, USER, false),
+        Arguments.of(true, fileUploadServiceName, "get", File.UploadState.COMPLETE, false, false, DATAMANAGER, false),
+        Arguments.of(true, fileUploadServiceName, "get", File.UploadState.COMPLETE, false, false, USER, true));
+  }
+
   @ParameterizedTest
-  @MethodSource("provideArgumentsForAuthorization")
+  @MethodSource("provideArgumentsForDatasetVersionServiceAuthorization")
   void testCheckAuthorizationForDatasetVersionService(boolean authorized, String serviceName, String methodName, State state,
       boolean isCreator, Role role, boolean hasReviewToken) {
     //Given
-    DatasetVersion datasetVersion = DatasetVersionBuilder.aDatasetVersion().state(state).build();
     Dataset dataset = DatasetBuilder.aDataset().state(state).id(UUID.randomUUID()).build();
+    DatasetVersion datasetVersion = DatasetVersionBuilder.aDatasetVersion().dataset(dataset).state(state).build();
 
     UserAccount userAccount = UserAccountBuilder.anUserAccount()
         .grants(Collections.singletonList(GrantBuilder.aGrant().role(role).dataset(dataset.getId()).build())).id(UUID.randomUUID()).build();
@@ -108,7 +126,6 @@ class AuthorizationServiceTest {
     if (isCreator) {
       dataset.setCreator(userAccount);
     }
-    datasetVersion.setDataset(dataset);
 
     if (hasReviewToken) {
       ReviewToken reviewToken = ReviewTokenBuilder.aReviewToken().token("TokenString").dataset(dataset.getId()).build();
@@ -119,6 +136,47 @@ class AuthorizationServiceTest {
     //When
     ThrowingCallable checkAuthorizationCode =
         () -> this.authorizationService.checkAuthorization(serviceName, methodName, r2D2Principal, datasetVersion);
+
+    //Then
+    if (authorized) {
+      assertThatCode(checkAuthorizationCode).doesNotThrowAnyException();
+    } else {
+      assertThatCode(checkAuthorizationCode).isInstanceOf(AuthorizationException.class);
+    }
+  }
+
+  @ParameterizedTest
+  @MethodSource("provideArgumentsForFileUploadServiceAuthorization")
+  void testCheckAuthorizationForFileUploadService(boolean authorized, String serviceName, String methodName, File.UploadState state,
+      boolean isFileCreator, boolean isDatasetCreator, Role role, boolean hasReviewToken) {
+    //Given
+    Dataset dataset = DatasetBuilder.aDataset().id(UUID.randomUUID()).build();
+    DatasetVersion datasetVersion = DatasetVersionBuilder.aDatasetVersion().dataset(dataset).build();
+
+    UserAccount userAccount = UserAccountBuilder.anUserAccount()
+        .grants(Collections.singletonList(GrantBuilder.aGrant().role(role).dataset(dataset.getId()).build())).id(UUID.randomUUID()).build();
+    R2D2Principal r2D2Principal = R2D2PrincipalBuilder.aR2D2Principal("userName", "pw", new ArrayList<>()).userAccount(userAccount).build();
+
+    File file = FileBuilder.aFile().state(state).datasets(Collections.singleton(datasetVersion)).build();
+
+    if (isFileCreator) {
+      file.setCreator(userAccount);
+    }
+
+    if (isDatasetCreator) {
+      //TODO: Why must user be creator of the Dataset and not the DatasetVersion in this case?
+      dataset.setCreator(userAccount);
+    }
+
+    if (hasReviewToken) {
+      ReviewToken reviewToken = ReviewTokenBuilder.aReviewToken().token("TokenString").dataset(dataset.getId()).build();
+      Mockito.when(this.reviewTokenRepository.findByToken(Mockito.any())).thenReturn(Optional.of(reviewToken));
+      r2D2Principal.setReviewToken(reviewToken.getToken());
+    }
+
+    //When
+    ThrowingCallable checkAuthorizationCode =
+        () -> this.authorizationService.checkAuthorization(serviceName, methodName, r2D2Principal, file);
 
     //Then
     if (authorized) {
