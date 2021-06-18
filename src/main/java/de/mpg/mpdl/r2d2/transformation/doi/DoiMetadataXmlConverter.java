b49bcb70c58d4484af17634637de83356ec83445
helkv <helk@mpdl.mpg.de>
2021-06-18 09:32:54 +0200

    RD-8 Use the new DataCite Metadata Schema 4.4
    - Replace DataCite Metadata Schema 4.3 with new version 4.4
    - Without adding/changing the metadata fields

--- src/main/java/de/mpg/mpdl/r2d2/transformation/doi/DoiMetadataXmlConverter.java
+++ src/main/java/de/mpg/mpdl/r2d2/transformation/doi/DoiMetadataXmlConverter.java
@@ -1,9 +1,7 @@
 package de.mpg.mpdl.r2d2.transformation.doi;
 
-import de.mpg.mpdl.r2d2.transformation.doi.model.DoiMetadata;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.stereotype.Component;
+import java.io.StringReader;
+import java.io.StringWriter;
 
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
@@ -11,16 +9,19 @@ import javax.xml.bind.Marshaller;
 import javax.xml.bind.Unmarshaller;
 import javax.xml.transform.Result;
 import javax.xml.transform.stream.StreamResult;
-import java.io.StringReader;
-import java.io.StringWriter;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import de.mpg.mpdl.r2d2.transformation.doi.model.DoiMetadata;
 
 @Component
 public class DoiMetadataXmlConverter {
 
+  public static final String DATACITE_METADATA_SCHEMA_4_4_LOCATION =
+      "http://datacite.org/schema/kernel-4 http://schema.datacite.org/meta/kernel-4.4/metadata.xsd";
   private static Logger LOGGER = LoggerFactory.getLogger(DoiMetadataXmlConverter.class);
-
-  public static final String DATACITE_METADATA_SCHEMA_4_3_LOCATION =
-      "http://datacite.org/schema/kernel-4 http://schema.datacite.org/meta/kernel-4.3/metadata.xsd";
 
   public String convertToXML(DoiMetadata doiMetadata) throws JAXBException {
     JAXBContext context = JAXBContext.newInstance(DoiMetadata.class);
@@ -30,7 +31,7 @@ public class DoiMetadataXmlConverter {
     Result result = new StreamResult(stringWriter);
 
     marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
-    marshaller.setProperty(Marshaller.JAXB_SCHEMA_LOCATION, DATACITE_METADATA_SCHEMA_4_3_LOCATION);
+    marshaller.setProperty(Marshaller.JAXB_SCHEMA_LOCATION, DATACITE_METADATA_SCHEMA_4_4_LOCATION);
     marshaller.marshal(doiMetadata, result);
 
     String doiMetadataXml = stringWriter.toString();
